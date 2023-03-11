b4dc56a1d9411d4e468249b8529c38f0bac74bad
astleymasvingise <85708015+astleytech@users.noreply.github.com>
2023-03-11 12:00:49 +0200

    Update app.py

--- app.py
+++ app.py
@@ -80,7 +80,6 @@ def processed_image(image_path):
   return tf.keras.applications.inception_v3.preprocess_input(img_array_expanded_dims)
 
 def saved_inception_v3(processed_image):
-def saved_inception_v3(processed_image):
   new_model = load_model('models/mode.h5')
   prediction = new_model.predict(processed_image)
   return np.round(prediction)
